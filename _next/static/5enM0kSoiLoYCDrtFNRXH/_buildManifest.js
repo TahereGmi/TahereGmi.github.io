095461acd706f88c46b3036e999d267ed95386f3
TahereGmi <mstgholami@gmail.com>
2023-06-09 02:50:18 +0330

    Deploy

---
+++ _next/static/5enM0kSoiLoYCDrtFNRXH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/828d9fdb80b5f1d2.css","static/chunks/pages/index-a49fad715c8ac1d5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
