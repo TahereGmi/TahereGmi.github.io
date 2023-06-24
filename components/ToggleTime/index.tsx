cf9bb84f10bd30ae962f0fdb8592c204a24a3ecd
TahereGmi <mstgholami@gmail.com>
2023-06-24 23:53:47 +0330

    Add toggle theme button

--- components/ToggleTime/index.tsx
+++ components/ToggleTime/index.tsx
@@ -12,14 +12,16 @@ const ToggleTime: React.FC<Props> = ({ toggleToNight, onClick }) => {
             className={`${styles.toggleWrapper} ${toggleToNight ? styles.nightDesign : styles.dayDesign}`}>
             {toggleToNight ? 
                 <>
-                    <span className={styles.tinyMoon} />
+                    {/* <span className={styles.tinyMoon} />
                     <span className={styles.moonCerved} />
                     <span className={styles.star1} />
-                    <span className={styles.star2} />
+                    <span className={styles.star2} /> */}
+                    Night
                 </> :
                 <>
-                    <span className={styles.tinySun} />
-                    <span className={styles.sunShine} />
+                    {/* <span className={styles.tinySun} />
+                    <span className={styles.sunShine} /> */}
+                    Day
                 </>}
         </div>
     )
