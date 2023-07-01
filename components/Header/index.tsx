1c62e33aa67c6dbb1ac24b6e3c8e7ee349e014e8
TahereGmi <mstgholami@gmail.com>
2023-07-01 23:50:04 +0330

    Add smooth scroll for heder links

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,41 @@
-import { FC } from 'react';
-interface IHeaderprops {};
+import React, { useEffect } from 'react';
+import styles from './header.module.scss';
 
-const Header: FC<IHeaderprops> = () => {
-    return (
-        <header>
-            <a href="#home">Home</a>
-            <a href="#contactMe">Contact</a>
-        </header>
-    )
+const Header: React.FC = () => {
+  useEffect(() => {
+    const smoothLinks = document.getElementsByClassName('smooth-link');
+
+    const handleClick = (event: Event) => {
+      event.preventDefault();
+
+      const targetId = (event.target as HTMLAnchorElement).getAttribute('href')?.substring(1);
+      const targetElement = document.getElementById(targetId || '');
+
+      console.log('Link clicked! Scrolling to ' + targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+
+    for (let i = 0; i < smoothLinks.length; i++) {
+      smoothLinks[i].addEventListener('click', handleClick);
+    }
+
+    return () => {
+      for (let i = 0; i < smoothLinks.length; i++) {
+        smoothLinks[i].removeEventListener('click', handleClick);
+      }
+    };
+  }, []);
+
+  return (
+    <nav className={styles.header}>
+      <a href="#home" className="smooth-link">Home</a>
+      <a href="#about" className="smooth-link">About</a>
+      <a href="#contact" className="smooth-link">Contact</a>
+    </nav>
+  );
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import Introduction from '@/components/Introduction';
 import Header from '@/components/Header';
+import styles from './home.module.scss';
 
 
 const Home: FC = () =>{
@@ -24,6 +25,9 @@ const Home: FC = () =>{
   return (
   <main className={`flex min-h-screen flex-col items-center justify-between`}>
     <Header />
+    <div id="home" className={styles.home}>Home</div>
+    <div id="about" className={styles.about}>about</div>
+    <div id="contact" className={styles.contact}>Contact</div>
     <Introduction />
   </main>
   )
