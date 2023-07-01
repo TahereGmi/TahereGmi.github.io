6a79b06ba35fa232152544dff39c354d706be683
TahereGmi <mstgholami@gmail.com>
2023-07-01 23:08:58 +0330

    Add contact me component

---
+++ components/ContactMe/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import styles from './contactMe.module.scss';
+
+interface Props {};
+
+const ContactMe: FC<Props> = () => {
+    return (
+        <div className={styles.contactWrapper}>
+            <span className={styles.contactMe}>Contact Me</span>
+            <span>Email</span>
+            <span>Github</span>
+            <span>Medium</span>
+            <span>LinkedIn</span>
+        </div>
+    )
+};
+
+export default ContactMe;
