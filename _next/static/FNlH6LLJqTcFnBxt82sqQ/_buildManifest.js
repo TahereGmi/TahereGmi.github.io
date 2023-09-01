6875806977ba9dd645e17d4c1b9d76e2aab1cd1f
TahereGmi <mstgholami@gmail.com>
2023-09-01 19:07:56 +0330

    Deploy

--- _next/static/FNlH6LLJqTcFnBxt82sqQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bbae7ba3b1792719.css","static/chunks/pages/index-608594301bec0f5c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/u2fNXPhQS1nre_jrwso1E/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4a63a0bd26284857.css","static/chunks/pages/index-adf3f3b33e2d974f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
